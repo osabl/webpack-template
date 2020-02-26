3b092eedb7943921fe3ecd525ab91a2062923e73
osabl <andrii.osabliuk@gmail.com>
2020-02-26 17:35:56 +0200

    reduce code to standardjs

--- webpack/webpack.build.conf.js
+++ webpack/webpack.build.conf.js
@@ -1,16 +1,16 @@
-const merge = require('webpack-merge');
-const baseWebpackConfig = require('./webpack.base.conf');
+const merge = require('webpack-merge')
+const baseWebpackConfig = require('./webpack.base.conf')
 
 const buildWebpackConfig = merge(baseWebpackConfig, {
-    mode: "production",
+  mode: 'production',
 
-    output: {
-        filename: `${PATHS.assets}js/[name].[hash].js`,
-        path: PATHS.dist,
-        publicPath: '/'
-    },
+  output: {
+    filename: `${baseWebpackConfig.externals.paths.assets}js/[name].[hash].js`,
+    path: baseWebpackConfig.externals.paths.dist,
+    publicPath: '/'
+  }
 })
 
 module.exports = new Promise((resolve, reject) => {
-    resolve(buildWebpackConfig);
+  resolve(buildWebpackConfig)
 })
--- webpack/webpack.dev.conf.js
+++ webpack/webpack.dev.conf.js
@@ -1,48 +1,43 @@
-const ifaces = require('os').networkInterfaces();
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-const baseWebpackConfig = require('./webpack.base.conf');
-
+const ifaces = require('os').networkInterfaces()
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const baseWebpackConfig = require('./webpack.base.conf')
 
 const devWebpackConfig = {
-    mode: "development",
-    devtool: 'eval-cheap-module-source-map',
-
-    output: {
-        filename: `${baseWebpackConfig.externals.paths.assets}js/[name].js`,
-        path: baseWebpackConfig.externals.paths.dist,
-        publicPath: '/'
-    },
-
-    devServer: {
-        contentBase: baseWebpackConfig.externals.paths.dist,
-        host: getMyIpV4Address(ifaces),
-        port: '8081',
-        overlay: true
-    },
-
-    plugins: [
-        new webpack.SourceMapDevToolPlugin({
-            filename: '[file].map'
-        })
-    ]
+  mode: 'development',
+  devtool: 'eval-cheap-module-source-map',
+  output: {
+    filename: `${baseWebpackConfig.externals.paths.assets}js/[name].js`,
+    path: baseWebpackConfig.externals.paths.dist,
+    publicPath: '/'
+  },
+  devServer: {
+    contentBase: baseWebpackConfig.externals.paths.dist,
+    host: getMyIpV4Address(ifaces),
+    port: '8081',
+    overlay: true
+  },
+  plugins: [
+    new webpack.SourceMapDevToolPlugin({
+      filename: '[file].map'
+    })
+  ]
 }
 
-
 module.exports = new Promise((resolve, reject) => {
-    resolve(merge(baseWebpackConfig, devWebpackConfig));
+  resolve(merge(baseWebpackConfig, devWebpackConfig))
 })
 
-function getMyIpV4Address(ifaces) {
-    let myIpV4Address;
+function getMyIpV4Address (ifaces) {
+  let myIpV4Address
 
-    Object.keys(ifaces).forEach(dev => {
-        ifaces[dev].filter(details => {
-            if (details.family === 'IPv4' && details.internal === false) {
-                myIpV4Address = details.address;
-            }
-        });
-    });
+  Object.keys(ifaces).forEach(dev => {
+    ifaces[dev].filter(details => {
+      if (details.family === 'IPv4' && details.internal === false) {
+        myIpV4Address = details.address
+      }
+    })
+  })
 
-    return myIpV4Address;
+  return myIpV4Address
 }
